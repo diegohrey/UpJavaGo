80806e69a626201b80c446b35ca6de8e192a39f1
Diego Rey <diegohrey@gmail.com>
2022-08-24 19:10:45 -0500

    proyecto ingreso egreso

---
+++ src/test/java/com/upjavago/ingresoegreso/IngresoegresoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upjavago.ingresoegreso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngresoegresoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
