51e9383a1d7e2eeea90ddd08ebfac5769a16b206
Diego Rey <diegohrey@gmail.com>
2022-09-03 22:20:07 -0500

    prueba

--- src/main/java/com/upjavago/ingresoegreso/IngresoegresoApplication.java
+++ src/main/java/com/upjavago/ingresoegreso/IngresoegresoApplication.java
@@ -1,13 +1,20 @@
 package com.upjavago.ingresoegreso;
 
+import com.upjavago.ingresoegreso.entity.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IngresoegresoApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(IngresoegresoApplication.class, args);
+
+		Empresa Cococola = new Empresa(1, "Cococola", "Cra 12 con 23", "321232345", "90053442133");
+		System.out.println(Cococola.getDireccion());
+
 	}
 
 }
